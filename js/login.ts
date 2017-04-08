260566291b55e670c11a4c8c122a974953049e54
Osmir <osmirmariano@uft.edu.br>
2017-04-07 23:51:19 -0300

    site para publicação de notícias

---
+++ js/login.ts
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.6.9/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCT5QhqGe8tXBhcCwXE27VUoWyngERdJYM",
+    authDomain: "appdai-beta.firebaseapp.com",
+    databaseURL: "https://appdai-beta.firebaseio.com",
+    storageBucket: "appdai-beta.appspot.com",
+    messagingSenderId: "562100545330"
+  };
+  firebase.initializeApp(config);
+</script>
