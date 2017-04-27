07aa5facda7057c67b01ac6f6324ae6924e31d53
Osmir <osmirmariano@uft.edu.br>
2017-04-27 19:43:25 -0300

    Organizando o site para organizar o banco de dados

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var app = angular.module('myApp', ['ngRoute', 'firebase']);
+
+
+app.config(function($routeProvider){
+  $routeProvider
+  	.when('/', {
+		controller: 'listarController',
+		templateUrl: 'views/listar.html'
+	})
+  	.when('/login', {
+  		controller: 'loginController',
+  		templateUrl: 'views/login.html'
+  	})
+	.when('/adicionar', {
+		controller: 'adicionarController',
+		templateUrl: 'views/adicionar.html'
+	})
+	.when('/editar/:id', {
+		controller: 'editarController',
+		templateUrl: 'views/editar.html'
+	
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
+
+app.constant("FBURL", "https://appdai-beta.firebaseio.com/noticias");
+
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://appdai-beta.firebaseio.com/noticias");
+    return $firebaseAuth(ref);
+  }
+]);
+
